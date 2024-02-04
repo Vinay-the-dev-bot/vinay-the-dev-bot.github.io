d03fdf3f656c2fbbb3017ad596b5af7b64cd6923
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-04 20:42:42 +0530

    Nav menu closes upon opening link

--- index.js
+++ index.js
@@ -62,3 +62,38 @@ cont.addEventListener("click", () => {
 // });
 
 // x.classList.toggle("change");
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    cont.classList.toggle("open");
+    const bar1 = document.getElementsByClassName("bar1")[0];
+    const bar2 = document.getElementsByClassName("bar2")[0];
+    const bar3 = document.getElementsByClassName("bar3")[0];
+    // console.log("Before", bar1, bar2, bar3);
+    bar1.classList.toggle("change");
+    bar2.classList.toggle("change");
+    bar3.classList.toggle("change");
+    // console.log("After", bar1, bar2, bar3);
+
+    let menu = document.getElementsByClassName("menu")[0];
+    if (!menu.style.display || menu.style.display == "none") {
+      menu.style.display = "flex";
+      menu.style.flexDirection = "column";
+      menu.style.alignItems = "center";
+    } else {
+      menu.style.display = "none";
+    }
+    let navLeft = document.getElementsByClassName("navLeft")[0];
+    if (!navLeft.style.flexDirection || navLeft.style.flexDirection == "row") {
+      document.getElementById("namelogo").style.paddingLeft = "50px";
+      navLeft.style.display = "flex";
+      navLeft.style.flexDirection = "column";
+      navLeft.style.alignItems = "center";
+    } else {
+      navLeft.style.display = "flex";
+      navLeft.style.flexDirection = "row";
+    }
+  });
+});
