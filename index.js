91f2c3286404d2ccf009821d289eb37a1cda505d
Vinay Meti <vinaygouda.meti16@gmail.com>
2024-02-05 21:17:35 +0530

    added responsiveness for tablet devices

--- index.js
+++ index.js
@@ -42,26 +42,10 @@ cont.addEventListener("click", () => {
   if (!navLeft.style.flexDirection || navLeft.style.flexDirection == "row") {
     document.getElementById("namelogo").style.paddingLeft = "50px";
     navLeft.style.display = "flex";
-    navLeft.style.flexDirection = "column";
-    navLeft.style.alignItems = "center";
   } else {
     navLeft.style.display = "flex";
-    navLeft.style.flexDirection = "row";
   }
 });
-// const cont = document.getElementById("container");
-// cont.addEventListener("click", () => {
-//   const bar1 = document.querySelector(".bar1");
-//   const bar2 = document.querySelector(".bar2");
-//   const bar3 = document.querySelector(".bar3");
-//   console.log("Before", bar1, bar2, bar3);
-//   bar1.classList.toggle("change");
-//   bar2.classList.toggle("change");
-//   bar3.classList.toggle("change");
-//   console.log("After", bar1, bar2, bar3);
-// });
-
-// x.classList.toggle("change");
 
 const navLinks = document.querySelectorAll(".nav-link");
 
@@ -71,29 +55,18 @@ navLinks.forEach((link) => {
     const bar1 = document.getElementsByClassName("bar1")[0];
     const bar2 = document.getElementsByClassName("bar2")[0];
     const bar3 = document.getElementsByClassName("bar3")[0];
-    // console.log("Before", bar1, bar2, bar3);
-    bar1.classList.toggle("change");
-    bar2.classList.toggle("change");
-    bar3.classList.toggle("change");
-    // console.log("After", bar1, bar2, bar3);
-
-    let menu = document.getElementsByClassName("menu")[0];
-    if (!menu.style.display || menu.style.display == "none") {
-      menu.style.display = "flex";
-      menu.style.flexDirection = "column";
-      menu.style.alignItems = "center";
-    } else {
-      menu.style.display = "none";
+    console.log("Before", bar1.classList.contains("change"));
+    if (bar1.classList.contains("change")) {
+      bar1.classList = "bar1";
     }
-    let navLeft = document.getElementsByClassName("navLeft")[0];
-    if (!navLeft.style.flexDirection || navLeft.style.flexDirection == "row") {
-      document.getElementById("namelogo").style.paddingLeft = "50px";
-      navLeft.style.display = "flex";
-      navLeft.style.flexDirection = "column";
-      navLeft.style.alignItems = "center";
-    } else {
-      navLeft.style.display = "flex";
-      navLeft.style.flexDirection = "row";
+    if (bar2.classList.contains("change")) {
+      bar2.classList = "bar2";
     }
+    if (bar3.classList.contains("change")) {
+      bar3.classList = "bar3";
+    }
+    // console.log("After", bar1.classList.contains("change"));
+    const menu = document.getElementsByClassName("menu")[0];
+    menu.style.display = "none";
   });
 });
